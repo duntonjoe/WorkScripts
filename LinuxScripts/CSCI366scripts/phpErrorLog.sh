bb044dc6e0c491dac0202fb37661cfc02f578505
Joe Dunton <duntonjoe@gmail.com>
2019-08-16 11:30:40 -0400

    fixed typo in phpErrorLog.sh

--- LinuxScripts/CSCI366scripts/phpErrorLog.sh
+++ LinuxScripts/CSCI366scripts/phpErrorLog.sh
@@ -2,7 +2,7 @@
 
 cd /etc/php/7.0/apache2
 rm php.ini
-scp jmdunotn.millersville.edu /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2
+scp jmdunton.millersville.edu /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2
 touch /var/log/php-errors.log
 chown www-data:www-data /var/log/php-errors.log
 systemctl restart apache2.service
